c6b0181b021e75c5305a24df0d50941f2153ec95
Johan <SteelRaven7@gmail.com>
2025-01-26 10:38:05 +0100

    Audio mixing and added ambient sounds

---
+++ Assets/Scripts/Testing/TestAudio.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAudio : MonoBehaviour
+{
+    public LayeredAudio layeredAudio;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            layeredAudio.TransitionToLayer(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            layeredAudio.TransitionToLayer(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            layeredAudio.TransitionToLayer(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            layeredAudio.TransitionToLayer(3);
+        }
+    }
+}
